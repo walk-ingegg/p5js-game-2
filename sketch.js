bcc185175af74a03c0e65a533f69d290f7977327
walk-ingegg <walkingegg.8814919@gmail.com>
2022-07-31 00:47:25 +0900

    add move ellipse by mouse

--- sketch.js
+++ sketch.js
@@ -1,8 +1,75 @@
+let wavePoint;
+
 function setup() {
   createCanvas(800, 800);
   background(0);
+  wavePoint = new WavePoint(50, 50, 2, 30);
 }
 
 function draw() {
-  ellipse(width / 2, height / 2, 100);
+  background(0);
+  wavePoint.draw();
+}
+
+function mousePressed() {
+  wavePoint.pressed();
+}
+
+function mouseReleased() {
+  wavePoint.notPressed();
+}
+
+class WavePoint {
+  constructor(x, y, speed, collisionSize) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.collisionSize = collisionSize / 2;
+    this.dragging = false;
+  }
+
+  draw() {
+    if (this.dragging) {
+      const mouseAreaX = width / 2;
+      const mouseAreaY = height / 2;
+      let moveX = this.x - mouseX;
+      let moveY = this.y - mouseY;
+
+      moveX = map(moveX, -mouseAreaX, mouseAreaX, -this.speed, this.speed);
+      moveY = map(moveY, -mouseAreaY, mouseAreaY, -this.speed, this.speed);
+      this.x -= moveX;
+      this.y -= moveY;
+    }
+    ellipse(this.x, this.y, 10);
+  }
+
+  pressed() {
+    if (
+      mouseX > this.x - this.collisionSize &&
+      mouseX < this.x + this.collisionSize &&
+      mouseY > this.y - this.collisionSize &&
+      mouseY < this.y + this.collisionSize
+    ) {
+      this.dragging = true;
+    }
+  }
+
+  notPressed() {
+    this.dragging = false;
+  }
+
+  movePositionKey() {
+    if (keyIsDown(RIGHT_ARROW)) {
+      this.x += this.speed;
+    }
+    if (keyIsDown(LEFT_ARROW)) {
+      this.x -= this.speed;
+    }
+    if (keyIsDown(UP_ARROW)) {
+      this.y -= this.speed;
+    }
+    if (keyIsDown(DOWN_ARROW)) {
+      this.y += this.speed;
+    }
+  }
 }
